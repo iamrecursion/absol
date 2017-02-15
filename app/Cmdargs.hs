c3ee6a62048b2a05c1278276771e59b3b095e1da
Ara Adkins <Ara Adkins>
2017-02-15 14:45:38 +0000

    Completed implementation of the CLI args parser.

--- app/Cmdargs.hs
+++ app/Cmdargs.hs
@@ -1,148 +1,138 @@
+{-|
+Module      : Cmdargs
+Description : Functionality for parsing the command-line arguments of the ABSOL
+              metacompiler.
+Copyright   : (c) Ara Adkins, 2017
+License     : Closed
+Maintainer  : me@ara.io
+Stability   : experimental
+Portability : GHC
 
-{-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE DisambiguateRecordFields #-}
-
+This module provides functionality for parsing the metacompiler command-line 
+arguments, as well as the data-type for storing them. 
+-}
 module Cmdargs
-    -- (
-    --     -- CLIOptions(..),
-    --     Options(..),
-    --     Command,
-    --     App,
-    --     Version,
-    --     URL,
-    --     BuildID,
-    --     parseCLIOptions,
-    --     withParserInfo
-    -- ) where
-    where
+    (
+        CLIOptions(..),
+        parseCLIOptions,
+        withParserInfo,
+        Options.Applicative.execParser
+    ) where
 
 import           Options.Applicative
 
-import           Data.Text
+type CmdLineFlag = Bool
+type File = FilePath
+type CmdString = Maybe String
 
--- | Obtains parser information and help text from the generated parser function
+-- | Storage for the command-line configuration.
 --
--- It is applied to a Parser and is able to generate a basic help text from the
--- static definition of that parser.
-withParserInfo :: Parser a -> String -> ParserInfo a
-withParserInfo options description =
-    info (helper <*> options) $ progDesc description
-
-
-
-type SpecFile = FilePath
-type ReportFlag = Bool
-type CleanFlag = Bool
-type VerboseFlag = Bool
-type SpecName = Text
-type SpecVersion = Text
-type LogFile = FilePath
-
+-- This type is filled with the parsed options at program start.
 data CLIOptions = CLIOptions {
-    inputFile :: SpecFile,
-    reportCheck :: ReportFlag,
-    cleanFiles :: CleanFlag,
-    verboseReporting :: VerboseFlag,
-    languageName :: SpecName,
-    languageVersion :: SpecVersion,
-    logFilePath :: LogFile
-}  deriving (Eq, Show)
+    filename    :: File,
+    reportFlag  :: CmdLineFlag,
+    cleanFlag   :: CmdLineFlag,
+    langName    :: CmdString,
+    langVersion :: CmdString,
+    verboseFlag :: CmdLineFlag,
+    logFile     :: Maybe File
+} deriving (Eq, Show)
 
-defaultCLIOptions :: CLIOptions
-defaultCLIOptions = CLIOptions {
-    inputFile = undefined,
-    reportCheck = False,
-    cleanFiles = False,
-    verboseReporting = False,
-    languageName = undefined,
-    languageVersion = undefined,
-    logFilePath = "./absol.log"
-}
+-- | A utility function for specifying the program description for the CLI help.
+-- 
+-- It is given a description and the program header text respectively.
+withParserInfo :: Parser a -> String -> String -> ParserInfo a
+withParserInfo options description headText =
+    info (helper <*> options) $
+    fullDesc <> progDesc description <> header headText
 
+-- | Parses the program's command-line options.
 parseCLIOptions :: Parser CLIOptions
-parseCLIOptions = undefined
+parseCLIOptions = CLIOptions
+    <$> parseFilename
+    <*> parseReportFlag
+    <*> parseCleanFlag
+    <*> parseLangName
+    <*> parseLangVersion
+    <*> parseVerboseFlag
+    <*> parseLogFile
 
-parseInputFile :: Parser SpecFile
-parseInputFile = argument str (metavar "INPUT-FILE")
+-- | Parses the input filepath from the CLI arguments.
+-- 
+-- This path specifies the metaspec file on which the metacompiler will operate.
+parseFilename :: Parser FilePath
+parseFilename = argument str (
+        metavar "FILENAME" <>
+        help "The metaspec file to run the metacomplier on. "
+    )
 
--- type NameString = Text
--- type VersionString = Text
--- type CleanFlag = Bool
--- type VerboseFlag = Bool
--- type SpecPath = FilePath
+-- | Parses the reporting flag from the CLI flags.
+-- 
+-- If this flag is set, the program will analyse the input file for semantic 
+-- correctness and then exit, without performing any codegen. 
+parseReportFlag :: Parser CmdLineFlag
+parseReportFlag = switch (
+        short 'r' <>
+        long "report" <>
+        help "Only print a report on the correctness of the input file."
+    )
 
--- -- | A type for containing the command-line options of the program.
--- data CLIOptions = CLIOptions {
---     path       :: SpecPath,
---     clean      :: CleanFlag,
---     name       :: NameString,
---     version    :: VersionString,
---     logVerbose :: VerboseFlag
--- } deriving (Eq, Show)
+-- | Parses the cleanup flag from the CLI arguments.
+-- 
+-- If specified, the program will delete all generated files. These files are
+-- determined from the language name and version.
+parseCleanFlag :: Parser CmdLineFlag
+parseCleanFlag = switch (
+        short 'c' <>
+        long "clean" <>
+        help "Delete metacompiler output. \
+        \ Output will be deleted by language name and version. This data\
+        \ can be provided by the --name and --version flags, or the input\
+        \ file "
+    )
 
--- type App = String
--- type Version = String
--- type URL = String
--- type BuildID = String
+-- | Parses the language name string from the CLI flags.
+--
+-- This name string is used to override the version specified in the input file.
+parseLangName :: Parser CmdString
+parseLangName = optional $ option str (
+        short 'n' <>
+        long "name" <>
+        help "Use the provided name STRING instead of the one embedded in\
+            \ the file." <>
+        metavar "STRING"
+    )
 
--- data Command
---     = Start URL Version
---     | Status BuildID
---     | Release BuildID App
+-- | Parses the language version string from the CLI flags.
+--
+-- This version string is used to override the version specified in the input
+-- file.
+parseLangVersion :: Parser CmdString
+parseLangVersion = optional $ option str (
+        short 'v' <>
+        long "version" <>
+        help "Use the provided version STRING instead of the one embedded in\
+            \ the file." <>
+        metavar "STRING"
+    )
 
--- data Options = Options App Command
+-- | Parses the verbosity flag from the CLI flags.
+--
+-- This flag sets whether the program will log verbose output.
+parseVerboseFlag :: Parser CmdLineFlag
+parseVerboseFlag = switch (
+        short 'l' <>
+        long "log-verbose" <>
+        help "Enable verbose logging."
+    )
 
--- parseCLIOptions :: Parser Options
--- parseCLIOptions = Options <$> parseApp <*> parseCommand
-
--- parseApp :: Parser App
--- parseApp = strOption
---     $ short 'a'
---     <> long "app"
---     <> metavar "COMPILE-APP"
---     <> help "The Application to compile"
-
--- parseCommand :: Parser Command
--- parseCommand = subparser $
---     command "start" (parseStart `withParserInfo` "Start a build.") <>
---     command "status" (parseStatus `withParserInfo` "Check build status.") <>
---     command "release" (parseRelease `withParserInfo` "Release build.")
-
--- parseStart :: Parser Command
--- parseStart = Start
---     <$> argument str (metavar "SOURCE-URL")
---     <*> argument str (metavar "VERSION")
-
--- parseStatus :: Parser Command
--- parseStatus = Status
---     <$> argument str (metavar "BUILD-ID")
-
--- parseRelease :: Parser Command
--- parseRelease = Release
---     <$> argument str (metavar "BUILD-ID")
---     <*> argument str (metavar "RELEASE-APP")
-
--- | Parses the command-line options for the program.
--- parseCLIOptions :: Parser CLIOptions
--- parseCLIOptions = CLIOptions
---     <$> parsePath
---     <*> parseCleanFlag
---     <*> parseLanguageName
---     <*> parseLanguageVersion
---     <*> parseVerboseFlag
-
--- parsePath :: Parser SpecPath
--- parsePath = path
---     <$> argument str (metavar "FILEPATH")
-
--- parseCleanFlag :: Parser CleanFlag
--- parseCleanFlag = undefined
-
--- parseLanguageName :: Parser NameString
--- parseLanguageName = undefined
-
--- parseLanguageVersion :: Parser VersionString
--- parseLanguageVersion = undefined
-
--- parseVerboseFlag :: Parser VerboseFlag
--- parseVerboseFlag = undefined
+-- | Parses the log file path from the CLI flags.
+--
+-- If this is set, the program will log to the specified file instead of stdout/
+-- stderr.
+parseLogFile :: Parser (Maybe FilePath)
+parseLogFile = optional $ option str (
+        long "log-file" <>
+        help "Log to the provided FILE." <>
+        metavar "FILE"
+    )
--- app/Main.hs
+++ app/Main.hs
@@ -1,118 +1,20 @@
 
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
--- import qualified Absol as A
--- import qualified Absol.Metalex as L
--- import qualified Absol.Metagen as G
--- import qualified Absol.Metaparse as P
--- import qualified Absol.Metaverify as V
+import           Cmdargs
 import qualified Data.Text.IO as TI
-import qualified Data.Text as T
+import           System.Exit
 
--- import Cmdargs
-
-import Options.Applicative 
-
--- Options
--- FILENAME : The name of the input metaspec file
--- [-r | --report] : Print a report on the safety of the language, but do not
--- generate any files.
--- [-c | --clean] : Remove any generated files (known from the name generation).
--- [-h | --help] : Print the help text.
--- [-n | --name NAME] : Override the name string contained in the file.
--- [-v | --version VERSION] : Override the version string contained in the file.
--- [-l | --log-verbose] : Set verbose logging mode.
--- [--log-file FILE] : Sets a log file. Otherwise defaults to stderr.
-
--- https://hackage.haskell.org/package/filepath-1.4.1.2/docs/System-FilePath-Posix.html
-
-type CmdLineFlag = Bool
-type File = FilePath
-type CmdString = Maybe String
-
-data CLIOptions = CLIOptions {
-    filename :: FilePath,
-    reportFlag :: CmdLineFlag,
-    cleanFlag :: CmdLineFlag,
-    langName :: CmdString,
-    langVersion :: CmdString,
-    verboseFlag :: CmdLineFlag,
-    logFile :: Maybe FilePath
-} deriving (Eq, Show)
-
-parseLangVersion :: Parser CmdString
-parseLangVersion = optional $ option str (
-        short 'v' <>
-        long "version" <>
-        help "Use the provided version instead of the one embedded \
-            \in the file." <>
-        metavar "STRING"
-    )
-
-parseVerboseFlag :: Parser CmdLineFlag
-parseVerboseFlag = switch (
-        short 'l' <>
-        long "log-verbose" <>
-        help "Enable verbose logging."
-    )
-
-parseLogFile :: Parser (Maybe FilePath) 
-parseLogFile = optional $ option str (
-        long "log-file" <>
-        help "Log to the provided FILE." <>
-        metavar "FILE"
-    )
-
-parseLangName :: Parser CmdString
-parseLangName = optional $ option str (
-        short 'n' <>
-        long "name" <>
-        help "Use the provided name instead of the one embedded in the file." <>
-        metavar "STRING"
-    )
-
-parseReportFlag :: Parser CmdLineFlag
-parseReportFlag = switch (
-        short 'r' <>
-        long "report" <>
-        help "Only print a report on the correctness of the input file."
-    )
- 
-parseFilename :: Parser FilePath
-parseFilename = argument str (metavar "FILE")
-
-parseCleanFlag :: Parser CmdLineFlag
-parseCleanFlag = switch (
-        short 'c' <>
-        long "clean" <>
-        help "Delete metacompiler products. \
-        \   Products will be deleted by language name and version. This data \
-        \   can be provided by the --name and --version flags, or the input \
-        \   file "
-    )
-
-parseCLIOptions :: Parser CLIOptions
-parseCLIOptions = CLIOptions
-    <$> parseFilename
-    <*> parseReportFlag
-    <*> parseCleanFlag
-    <*> parseLangName
-    <*> parseLangVersion
-    <*> parseVerboseFlag
-    <*> parseLogFile
-
--- Can I make this more flexible? Yeah, but no longer infix. Cool! TODO
-withParserInfo :: Parser a -> String -> ParserInfo a
-withParserInfo options description =
-    info (helper <*> options) $ progDesc description
-
+-- | The main function for ABSOL.
 main :: IO ()
 main = runMetacompiler =<< execParser (
-        parseCLIOptions `withParserInfo` "Run the ABSOL Metacompiler."
+        withParserInfo
+            parseCLIOptions
+            "Run the absol metacompiler."
+            "ABSOL :: Automatic Builder for Semantically Oriented Languages"
     )
 
+-- | Contains the main execution context of the metacompiler.
 runMetacompiler :: CLIOptions -> IO ()
 runMetacompiler opts = do
     TI.putStrLn "Woop!"
@@ -123,3 +25,4 @@ runMetacompiler opts = do
     putStrLn $ "LangVer " ++ show (langVersion opts)
     putStrLn $ "Verbose " ++ show (verboseFlag opts)
     putStrLn $ "LogFile " ++ show (logFile opts)
+    exitSuccess
