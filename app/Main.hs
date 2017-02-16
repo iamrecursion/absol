68b30aec4c4c7425ebb936c541355a98de64186b
Ara Adkins <Ara Adkins>
2017-02-16 15:44:48 +0000

    Initial work on the lexer.

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,11 @@
 
 module Main where
 
+import           Absol.Utilities (outputToken)
 import           Cmdargs
-import qualified Data.Text.IO as TI
 import           System.Exit
+import           System.IO
+import           System.IO.Error
 
 -- | The main function for ABSOL.
 main :: IO ()
@@ -14,15 +16,32 @@ main = runMetacompiler =<< execParser (
             "ABSOL :: Automatic Builder for Semantically Oriented Languages"
     )
 
+-- TODO Output directory argument.
+
 -- | Contains the main execution context of the metacompiler.
 runMetacompiler :: CLIOptions -> IO ()
-runMetacompiler opts = do
-    TI.putStrLn "Woop!"
-    putStrLn $ "Filename " ++ filename opts
-    putStrLn $ "Report " ++ show (reportFlag opts)
-    putStrLn $ "Clean " ++ show (cleanFlag opts)
-    putStrLn $ "LangName "  ++ show (langName opts)
-    putStrLn $ "LangVer " ++ show (langVersion opts)
-    putStrLn $ "Verbose " ++ show (verboseFlag opts)
-    putStrLn $ "LogFile " ++ show (logFile opts)
-    exitSuccess
+runMetacompiler opts@CLIOptions{filename=file, cleanFlag=False} = do
+    putStrLn $ outputToken ++ "Executing the ABSOL metacompiler on " ++ file
+    result <- tryIOError process
+    case result of
+        Left ex -> processFailure ex
+        Right _ -> processSuccess
+    return ()
+    where
+        process = acquireMetaspecFile file (processMetaspecFile opts)
+        processFailure ex = do
+            hPrint stderr ex
+            exitFailure :: IO ()
+        processSuccess = do
+            putStrLn $ outputToken ++ "Input file successfully processed."
+            exitSuccess :: IO ()
+runMetacompiler CLIOptions{cleanFlag=True} = undefined
+-- TODO cleanup functionality
+
+-- | Loads the metaspec file and executes the metacompiler processing on it.
+acquireMetaspecFile :: FilePath -> (Handle -> IO a) -> IO a
+acquireMetaspecFile file = withFile file ReadMode
+
+-- | The main processing chain for the metacompiler.
+processMetaspecFile :: CLIOptions -> Handle -> IO ()
+processMetaspecFile _ mFile = putStrLn =<< hGetContents mFile
---
+++ src/Absol/Metagen/Utilities.hs
@@ -0,0 +1,36 @@
+-------------------------------------------------------------------------------
+-- |
+-- Module      : Absol.Metagen.Utilities
+-- Description : Utilities to support code generation in Metagen.
+-- Copyright   : (c) Ara Adkins (2017)
+-- License     : See LICENSE file
+-- 
+-- Maintainer  : Ara Adkins
+-- Stability   : experimental
+-- Portability : GHC
+-- 
+-- Utilities supporting code generation in Metagen.
+--
+-------------------------------------------------------------------------------
+
+module Absol.Metagen.Utilities (
+        OutputFileNames(..),
+        generateOutputFileNames
+    ) where
+
+import qualified Data.Text as T
+import Data.Monoid ((<>))
+
+-- | This type defines the output filenames for the 
+data OutputFileNames = OutputFileNames {
+    targetCompilerSource :: T.Text,
+    targetCompilerBuildLog :: T.Text
+} deriving (Eq, Show)
+
+generateOutputFileNames :: String -> String -> OutputFileNames
+generateOutputFileNames name ver = let
+    namePart = T.pack $ name ++ "-" ++ ver
+    compilerSrc = namePart <> T.pack "-compiler.hs"
+    logFile = namePart <> T.pack "-build.log"
+    in
+        OutputFileNames compilerSrc logFile
