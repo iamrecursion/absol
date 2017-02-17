f2dbe3e766c35478fe4ffa74741b9d884da8b1fe
Ara Adkins <me@ara.io>
2017-02-17 16:53:24 +0000

    Initial work on the lexer.

---
+++ src/Absol/Metalex/Wrapper.hs
@@ -0,0 +1,21 @@
+-------------------------------------------------------------------------------
+-- |
+-- Module      : Absol.Metalex.Wrapper
+-- Description : Provides the wrapper interface around the Alex-based lexer. 
+-- Copyright   : (c) Ara Adkins (2017)
+-- License     : See LICENSE file
+-- 
+-- Maintainer  : Ara Adkins
+-- Stability   : experimental
+-- Portability : GHC
+-- 
+-- A wrapper around the the lexer of choice, allowing the lexer to be swapped 
+-- with a minimum of effort.
+--
+-------------------------------------------------------------------------------
+
+module Absol.Metalex.Wrapper (
+
+    ) where
+
+import Data.Monoid
